4adb0f11e94f8e666aee20fe3495870779291ac5
mrcnbsb <marconebsb@gmail.com>
2026-07-11 16:27:10 -0300

    First Steps in Java With Spring Boot

---
+++ rest-with-spring-boot-and-java-curso-erudio/src/test/java/mrcnbsb/StartupTests.java
@@ -0,0 +1,13 @@
+package mrcnbsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
