481dce471a23936f69a7c432aa7c00327200c87b
mrcnbsb <marconebsb@gmail.com>
2026-07-16 10:57:03 -0300

    Final seção 6

--- rest-with-spring-boot-and-java-curso-erudio/src/main/java/mrcnbsb/model/Greeting.java
+++
@@ -1,5 +0,0 @@
-package mrcnbsb.model;
-
-public record Greeting(long id, String content) {
-    
-}
---
+++ rest-with-spring-boot-and-java-curso-erudio/src/main/java/mrcnbsb/model/Person.java
@@ -0,0 +1,69 @@
+package mrcnbsb.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String addres;
+    private String gender;
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Person person)) return false;
+        return Objects.equals(getId(), person.getId()) && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getAddres(), person.getAddres()) && Objects.equals(getGender(), person.getGender());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getAddres(), getGender());
+    }
+}
